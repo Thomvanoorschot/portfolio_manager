41c1a3c860dae4bf060747f1fc7b12ccec2131b9
Thom <thomvanoorschot@hotmail.com>
2022-12-25 13:07:17 +0100

    NBow uses goroutines

--- app/charting/historical_data_handler.go
+++ app/charting/historical_data_handler.go
@@ -5,9 +5,45 @@ import (
 	"fmt"
 	"github.com/Thomvanoorschot/portfolioManager/app/helpers"
 	"github.com/Thomvanoorschot/portfolioManager/app/infrastructure"
+	"math"
 	"net/http"
+	"sync"
 	"time"
 )
+
+type safeHoldingsPerDay struct {
+	sync.RWMutex
+	holdingsPerDay map[time.Time]*safeHoldings
+}
+
+func (sn *safeHoldingsPerDay) Add(d time.Time, holdings *safeHoldings) {
+	sn.Lock()
+	defer sn.Unlock()
+	sn.holdingsPerDay[d] = holdings
+}
+
+func (sn *safeHoldingsPerDay) Get(d time.Time) *safeHoldings {
+	sn.RLock()
+	defer sn.RUnlock()
+	return sn.holdingsPerDay[d]
+}
+
+type safeHoldings struct {
+	sync.RWMutex
+	holdings map[string]*holding
+}
+
+func (sn *safeHoldings) Add(symbol string, holding *holding) {
+	sn.Lock()
+	defer sn.Unlock()
+	sn.holdings[symbol] = holding
+}
+
+func (sn *safeHoldings) Get(symbol string) *holding {
+	sn.RLock()
+	defer sn.RUnlock()
+	return sn.holdings[symbol]
+}
 
 type holding struct {
 	amount                 float64
@@ -28,8 +64,13 @@ func HistoricalDataHandler(server *infrastructure.Server, _ *http.Request, respo
 	uniqueSymbols := transactionRepository.GetUniqueSymbols()
 	historicalDataPerSymbol := server.UnitOfWork.HistoricalDataRepository.GetBySymbols(uniqueSymbols)
 
-	holdings := map[time.Time]map[string]*holding{}
-	holdings[start] = map[string]*holding{}
+	holdings := safeHoldingsPerDay{
+		holdingsPerDay: map[time.Time]*safeHoldings{},
+	}
+	holdings.holdingsPerDay[start] = &safeHoldings{
+		RWMutex:  sync.RWMutex{},
+		holdings: map[string]*holding{},
+	}
 	var resp [][]float64
 	for d := start; d.After(end) == false; d = d.AddDate(0, 0, 1) {
 		for _, transaction := range transactions {
@@ -37,42 +78,66 @@ func HistoricalDataHandler(server *infrastructure.Server, _ *http.Request, respo
 			if !truncatedTransactedAt.Equal(d) {
 				continue
 			}
-
-			if holdings[truncatedTransactedAt][transaction.Symbol] == nil {
-				holdings[truncatedTransactedAt][transaction.Symbol] = &holding{}
+			thisDaysHoldings := holdings.Get(truncatedTransactedAt)
+			thisDaysSymbolHoldings := thisDaysHoldings.Get(transaction.Symbol)
+			if thisDaysSymbolHoldings == nil {
+				newHolding := &holding{}
+				thisDaysHoldings.Add(transaction.Symbol, newHolding)
+				thisDaysSymbolHoldings = newHolding
 			}
-			holdings[truncatedTransactedAt][transaction.Symbol].amount += transaction.Amount
+			thisDaysSymbolHoldings.amount += transaction.Amount
 		}
 
 		var dayPrice float64
-		holdings[d.AddDate(0, 0, 1)] = map[string]*holding{}
-		for symbol, h := range holdings[d] {
-			holdings[d.AddDate(0, 0, 1)][symbol] = &holding{
-				amount:                 h.amount,
-				symbolPriceAtGivenTime: h.symbolPriceAtGivenTime,
-				total:                  h.total,
-			}
-			var symbolPriceAtGivenTime float64
-			for _, historicalData := range historicalDataPerSymbol[symbol] {
-				if historicalData.Timestamp.Year() == d.Year() && historicalData.Timestamp.Month() == d.Month() && historicalData.Timestamp.Day() == d.Day() {
-					symbolPriceAtGivenTime = historicalData.Close
-					break
-				}
-			}
-			if symbolPriceAtGivenTime == 0 {
-				previousDaysHolding := holdings[d.AddDate(0, 0, -1)]
-				s := previousDaysHolding[symbol]
-				if s != nil {
-					symbolPriceAtGivenTime = s.symbolPriceAtGivenTime
-				}
-				// TODO Deal with symbol changes not having historical data
-			}
-			h.symbolPriceAtGivenTime = symbolPriceAtGivenTime
-			h.total = symbolPriceAtGivenTime * h.amount
-			dayPrice += h.total
+		newSafeHoldings := &safeHoldings{
+			RWMutex:  sync.RWMutex{},
+			holdings: map[string]*holding{},
 		}
+		wg := sync.WaitGroup{}
+		currentHoldings := holdings.Get(d).holdings
+		c := make(chan float64, len(currentHoldings))
 
-		resp = append(resp, []float64{float64(d.UnixMilli()), dayPrice})
+		for symbol, h := range currentHoldings {
+			wg.Add(1)
+			go func(symbol string, h *holding, c chan float64) {
+				defer wg.Done()
+				var symbolPriceAtGivenTime float64
+				for _, historicalData := range historicalDataPerSymbol[symbol] {
+					if historicalData.Timestamp.Year() == d.Year() && historicalData.Timestamp.Month() == d.Month() && historicalData.Timestamp.Day() == d.Day() {
+						symbolPriceAtGivenTime = historicalData.Close
+						break
+					}
+				}
+				if symbolPriceAtGivenTime == 0 {
+					previousDaysHolding := holdings.Get(d.AddDate(0, 0, -1))
+					s := previousDaysHolding.Get(symbol)
+					if s != nil {
+						symbolPriceAtGivenTime = s.symbolPriceAtGivenTime
+					} else {
+						fmt.Println("Could not find price")
+					}
+					// TODO Deal with symbol changes not having historical data
+				}
+				h.symbolPriceAtGivenTime = symbolPriceAtGivenTime
+				h.total = symbolPriceAtGivenTime * h.amount
+				c <- h.total
+				newSafeHoldings.Add(symbol, &holding{
+					amount:                 h.amount,
+					symbolPriceAtGivenTime: h.symbolPriceAtGivenTime,
+					total:                  h.total,
+				})
+			}(symbol, h, c)
+		}
+		go func() {
+			defer close(c)
+			wg.Wait()
+		}()
+		for elem := range c {
+			dayPrice += elem
+		}
+		holdings.Add(d.AddDate(0, 0, 1), newSafeHoldings)
+
+		resp = append(resp, []float64{float64(d.UnixMilli()), math.Round(dayPrice*100) / 100})
 	}
 
 	marshal, err := json.Marshal(resp)
